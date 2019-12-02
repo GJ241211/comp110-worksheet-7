9dfc5c5724ab472572a8c6bf8728c11e25406eb2
gj241211 <gj241211@falmouth.ac.uk>
2019-12-02 18:08:59 +0000

    Changed pseudocode for solutions, still need to answer last 3.

--- comp110-worksheet-7/DirectoryUtils.cs
+++ comp110-worksheet-7/DirectoryUtils.cs
@@ -24,44 +24,68 @@ namespace comp110_worksheet_7
 		// Return the total size, in bytes, of all the files below the given directory
 		public static long GetTotalSize(string directory)
 		{
-
-
-
-            string[] files = Directory.GetFiles(directory, "*ProfileHandler.cs", SearchOption.AllDirectories); // search pattern? 
-
-            int Total_Size = 0;
-
-            int number_of_files = files.Length;  //start from midpoint? 
             
-            foreach (string file in files){
+            string[] files = Directory.GetFileSystemEntries(directory); // search pattern? 
 
-                int File_Size = 0; // 
-                Total_Size = Total_Size + File_Size;           
+            long Total_Size = 0;            
             
+            foreach (string element in files){
+                if (IsDirectory(element))
+                {
+                    Total_Size = Total_Size + GetTotalSize(element);
+                }
+                else{
+                    Total_Size = Total_Size + GetFileSize(element);
+                }                             
             }
 
 
-			throw new NotImplementedException();
+			return Total_Size;
 		}
 
 		// Return the number of files (not counting directories) below the given directory
 		public static int CountFiles(string directory)
 		{
 
+            string[] files = Directory.GetFileSystemEntries(directory); // search pattern? 
 
-            Directory.GetFiles(directory, "*ProfileHandler.cs", SearchOption.TopDirectoryOnly);   // same as above with just this line changed?
+            int file_count = 0;
 
-            throw new NotImplementedException();
+            foreach (string element in files)
+            {
+                if (IsDirectory(element))
+                {
+                    file_count = file_count + CountFiles(element);                   
+                }
+                else
+                {
+                    file_count ++; 
+                }
+            }
 
+
+            return file_count;                                          
         }
 
 		// Return the nesting depth of the given directory. A directory containing only files (no subdirectories) has a depth of 0.
 		public static int GetDepth(string directory)
 		{
+            int Current_Depth = 0;
 
-            // using GetDirectorys?
+            string[] Array_Of_Directories = Directory.GetDirectories(directory);
 
-            throw new NotImplementedException();
+            foreach (string element in Array_Of_Directories) {
+                
+                int New_Depth = GetDepth(element);
+                if (New_Depth > Current_Depth) {
+                    Current_Depth = New_Depth;
+                }            
+            }
+
+            //Console.WriteLine(Current_Depth++);
+            Current_Depth++;
+            return Current_Depth;
+            
                         
 		}
 
@@ -69,7 +93,6 @@ namespace comp110_worksheet_7
 		public static Tuple<string, long> GetSmallestFile(string directory)
 		{
 			throw new NotImplementedException();
-
 
 		}
 
