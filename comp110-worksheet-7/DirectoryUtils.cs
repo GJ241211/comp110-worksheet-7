d53f717ae9e6cbc6563572355fd8c19f409ff34a
gj241211 <gj241211@falmouth.ac.uk>
2019-12-02 12:18:02 +0000

    Added mostly pseudocode.

--- comp110-worksheet-7/DirectoryUtils.cs
+++ comp110-worksheet-7/DirectoryUtils.cs
@@ -24,25 +24,53 @@ namespace comp110_worksheet_7
 		// Return the total size, in bytes, of all the files below the given directory
 		public static long GetTotalSize(string directory)
 		{
+
+
+
+            string[] files = Directory.GetFiles(directory, "*ProfileHandler.cs", SearchOption.AllDirectories); // search pattern? 
+
+            int Total_Size = 0;
+
+            int number_of_files = files.Length;  //start from midpoint? 
+            
+            foreach (string file in files){
+
+                int File_Size = 0; // 
+                Total_Size = Total_Size + File_Size;           
+            
+            }
+
+
 			throw new NotImplementedException();
 		}
 
 		// Return the number of files (not counting directories) below the given directory
 		public static int CountFiles(string directory)
 		{
-			throw new NotImplementedException();
-		}
+
+
+            Directory.GetFiles(directory, "*ProfileHandler.cs", SearchOption.TopDirectoryOnly);   // same as above with just this line changed?
+
+            throw new NotImplementedException();
+
+        }
 
 		// Return the nesting depth of the given directory. A directory containing only files (no subdirectories) has a depth of 0.
 		public static int GetDepth(string directory)
 		{
-			throw new NotImplementedException();
+
+            // using GetDirectorys?
+
+            throw new NotImplementedException();
+                        
 		}
 
 		// Get the path and size (in bytes) of the smallest file below the given directory
 		public static Tuple<string, long> GetSmallestFile(string directory)
 		{
 			throw new NotImplementedException();
+
+
 		}
 
 		// Get the path and size (in bytes) of the largest file below the given directory
@@ -54,7 +82,15 @@ namespace comp110_worksheet_7
 		// Get all files whose size is equal to the given value (in bytes) below the given directory
 		public static IEnumerable<string> GetFilesOfSize(string directory, long size)
 		{
-			throw new NotImplementedException();
+
+            List<string> Files_That_Match = new List<string>();
+
+            // loop through all files adding the ones that match to a list,
+
+
+
+            throw new NotImplementedException();              
+
 		}
 	}
 }
